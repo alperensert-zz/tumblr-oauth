c695b718e80ceee30bb369dd4b3bc5ff523f7956
Alperen Sert <sertalperen99@gmail.com>
2017-10-01 12:47:36 +0300

    Update config.php

--- config.php
+++ config.php
@@ -7,6 +7,6 @@
 
 require_once "tumblrlib/tumblroauth.php";
 require "html.inc";
-define('CONSUMER_KEY', 'xhIeubNGQFCQFDf19S3TSiTQRbTzOsySRIH4nT6IPyJLHQ46JM');
-define('CONSUMER_SECRET', '0LuOaBmMvsmNqKamhqqBi8Yq7I9CrDPNTE0tWVmfN5SdHeQ40W');
-define('OAUTH_CALLBACK', 'http://localhost/callback.php');
+define('CONSUMER_KEY', 'yourconsumerkey');
+define('CONSUMER_SECRET', 'yourconsumersecretkey');
+define('OAUTH_CALLBACK', 'yourcallbackurl');
